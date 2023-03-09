082a657d293c0fe2352694795de1df292d07b404
Bang0123 <nikolajbang1995@gmail.com>
2023-03-09 20:21:06 +0100

    Add maui project

---
+++ flic2lib.Maui/FlicManager.cs
@@ -0,0 +1,12 @@
+﻿using IO.Flic.Flic2libandroid;
+
+namespace flic2lib.Maui;
+
+public partial class FlicManager
+{
+    public FlicManager()
+    {
+    }
+
+    partial FlicManager Initialize();
+}
